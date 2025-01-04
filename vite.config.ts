813f7feacf76d8562ec99ebc4f53116724c41792
Your Name <you@example.com>
2025-01-05 05:29:55 +0900

    viteの修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES  
-  ? "LocalTextViewer"            
-  : "./",  
+  base: process.env.GITHUB_PAGES ? "レポジトリ名" : "./",
   plugins: [react()],
   resolve: {
     alias: {
