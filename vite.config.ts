90273c4551cd9ee7aeef0f4570d713b781737f34
Your Name <you@example.com>
2025-01-06 01:14:15 +0900

    PWAになった？

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,39 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES ? "レポジトリ名" : "./",
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    includeAssets: ['assets/favicon.ico'],
+    manifest: {
+      name: 'Local Text Viewer',
+      short_name: 'Txt View',
+      description: 'View text files on your browser',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'assets/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'assets/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+
+],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
