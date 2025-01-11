05c8bc7e5ddb0bd628712c4d13f58111531bb7cc
Your Name <you@example.com>
2025-01-12 00:31:40 +0900

    境界線を書くかどうかは検討予知ある

--- src/header.tsx
+++ src/header.tsx
@@ -7,6 +7,7 @@ interface HeaderProps {
 
 export default function Header({ children }: HeaderProps) {
   return (
+    /*{ border-b の部分はなくてもいいかも}*/
 <header className="flex items-center  justify-end p-4 bg-background border-b">
         <div className="flex items-center space-x-2">
           <FileText className="h-5 w-5" />
